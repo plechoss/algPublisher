472f1fca9cbdc125fb831c4593186e40030e10e2
Michal Pleskowicz <plaskacz.panda@gmail.com>
2018-08-10 22:40:06 +0200

    Idk really

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 ‘use strict’;
 importScripts(‘sw-toolbox.js’);
-toolbox.precache([“index.html”,”css/style.css”, "js/init.js"]);
+toolbox.precache([“index.html”,”css/style.css”, ”js/init.js”, ”js/materialize.min.js”, ”css/materialize.min.css”]);
 toolbox.router.get(‘/images/*’, toolbox.cacheFirst);
 toolbox.router.get(‘/*’, toolbox.networkFirst, { networkTimeoutSeconds: 5});
