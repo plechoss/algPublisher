a57b0512945f65eb446d037748bb5b0e022087ea
Michal Pleskowicz <plaskacz.panda@gmail.com>
2018-08-10 22:55:51 +0200

    Same.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
 importScripts('sw-toolbox.js');
-toolbox.precache([“/algPublisher/index.html”,”/algPublisher/css/style.css”, ”/algPublisher/js/init.js”, ”/algPublisher/js/materialize.min.js”, ”/algPublisher/css/materialize.min.css”]);
+toolbox.precache(['/algPublisher/index.html','/algPublisher/css/style.css', '/algPublisher/js/init.js', '/algPublisher/js/materialize.min.js', '/algPublisher/css/materialize.min.css']);
 toolbox.router.get('/images/*', toolbox.cacheFirst);
 toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
